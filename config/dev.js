324c0483dd9a9d7321a4dd61a06148c4b4ef7bb7
yashdaga18 <email.yash.daga@gmail.com>
2023-10-05 03:28:57 +0530

    initial

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MONGO_URI : "mongodb+srv://yashdaga7019:KhHVuE9vk9xy5Zwu@cluster0.qk5xwfv.mongodb.net/varun",
+    JWT: 'lmao-noob'
+}
+//mongodb+srv://root:<password>@blogfeed.qekka.mongodb.net/blogfeed?retryWrites=true&w=majority
+//mongodb://root:blogfeed1234@ds339648.mlab.com:39648/blogfeed
